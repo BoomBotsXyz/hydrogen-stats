1373cdacb50c93932b751aa32690dadd534ea11b
wwHysenberg <wwhysenberg@protonmail.com>
2023-05-21 21:41:58 -0700

    fetch all events

---
+++ api/events/get.js
@@ -0,0 +1,49 @@
+// responsible for fetching all events of the nucleus
+
+const ethers = require("ethers")
+const BN = ethers.BigNumber
+const { s3GetObjectPromise, snsPublishError } = require("./../utils/aws")
+const { fetchNucleusState } = require("./../tracker/fetchNucleusState")
+const { verifyParams } = require("./verifyParams")
+
+// Define headers
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE"
+}
+
+async function handle(event) {
+  var { chainID } = verifyParams(event["queryStringParameters"])
+  await fetchNucleusState(chainID)
+  return await s3GetObjectPromise({ Bucket: "stats.hydrogen.hysland.finance.data", Key: `${chainID}/events.json` })
+}
+
+exports.handler = async function(event) {
+  try {
+    var res = await handle(event)
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: res
+    }
+  } catch (e) {
+    switch(e.name) {
+      case "InputError":
+        return {
+          statusCode: 400,
+          headers: headers,
+          body: e.stack
+        }
+        break
+      default:
+        await snsPublishError(event, e)
+        return {
+          statusCode: 500,
+          headers: headers,
+          body: "internal server error"
+        }
+    }
+  }
+}
---
+++ api/events/verifyParams.js
@@ -0,0 +1,23 @@
+const ethers = require("ethers")
+const BN = ethers.BigNumber
+const { getAddress } = ethers.utils
+const { getNetworkSettings } = require("./../utils/getNetworkSettings")
+
+function verifyChainID(params) {
+  var chainID = params["chainid"] || params["chainId"] || params["chainID"]
+  if(!chainID) throw { name: "InputError", stack: 'chainID not given' }
+  try {
+    var chainID2 = parseInt(chainID)
+    getNetworkSettings(chainID2)
+    return chainID2
+  } catch(e) {
+    throw { name: "InputError", stack: `chainID '${chainID}' not supported` }
+  }
+}
+
+function verifyParams(params) {
+  if(!params) throw { name: "InputError", stack: 'params not given' }
+  var chainID = verifyChainID(params)
+  return { chainID }
+}
+exports.verifyParams = verifyParams
